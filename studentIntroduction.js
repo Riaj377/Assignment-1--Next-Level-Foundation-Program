498e92e35605b161f6acde5de241753f46d2577d
Riaj377 <mdwakiurrahamanriaj@gmail.com>
2026-09-07 20:54:48 +0600

    Delete studentIntroduction.js

--- studentIntroduction.js
+++
@@ -1,9 +0,0 @@
-function studentIntroduction(student) {
-    // Write your code here
-  if( typeof student !== "object" || !("name" in student) || !("age" in student) || !("course" in student) || student === null ){
-    return "Invalid"
-  }
-    return `My name is ${student.name}. I am ${student.age} years old. I am learning ${student.course}.`
-}
-
-console.log(studentIntroduction({"name":"Rafi","age":18,"course":"JavaScript"}))
